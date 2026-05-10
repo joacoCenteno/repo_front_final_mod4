7947096fea9508fd4cc38dfa4139e0a0c3c6254f
joacoCenteno <centenojoaco01@gmail.com>
2026-05-09 22:25:49 -0300

    Axios file was modified

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://repo-back-final-mod4.onrender.com";
-axios.defaults.withCredentials = true
+// axios.defaults.baseURL = "https://repo-back-final-mod4.onrender.com";
+axios.defaults.baseURL = "http://localhost:3000"
+
 
 export default axios
