c644437b3214a861b558c15b0f27eeaea0c401c7
joacoCenteno <centenojoaco01@gmail.com>
2026-05-17 00:24:48 -0300

    General changes

--- src/components/EnvioMail.jsx
+++ src/components/EnvioMail.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuth } from '../contexts/AuthContext';
+import { useThemeContext } from '../contexts/ThemeContext';
+import { useForm } from 'react-hook-form';
 
 const EnvioMail = () => {
+    const {sendEmail} = useAuth();
+    const {isDark} = useThemeContext()
+    const [error, setError] = useState(null);
+    const [waiting, setWaiting] = useState(false)
+
+
+    const {register, handleSubmit, formState: {errors, isSubmitting}} = useForm({
+        mode: 'onChange'
+    })
+    
+
+    const onSubmit = async (data) =>{
+        setError("")
+        setWaiting(false);
+       await sendEmail(data.email);
+    }
+    
+
   return (
-    <div>EnvioMail</div>
+    <>
+        <h2 className={`font-bold text-2xl ${!isDark&&"text-[#4e5c77] "}`}>Envio de Email</h2>
+
+        
+
+        <form action="post" onSubmit={handleSubmit(onSubmit)} className={`flex flex-col gap-4  bg-[#171e2d] text-[#42c1fc] shadow-xl p-6 rounded-2xl w-full max-w-md mx-auto ${!isDark&&"text-[#4e5c77] bg-[#eceeff]"}
+             `}>
+              <p className='text-red-400 text-center'>{error}</p>
+            <input {...register('email', {
+                        required: 'Email es requerido',
+                        pattern: {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: "Formato de email inválido"
+                        }
+                    })} type="text" className={`border border-gray-600 rounded-lg px-3 py-2 focus:ring-2 focus:ring-[#42c1fc]  focus:outline-none ${!isDark&&"text-[#4e5c77]  focus:ring-[#b9c0ff] focus:border-none border-white"}`} placeholder='Email' />
+            {errors.email && <p className='text-red-400 text-sm'>{errors.email.message}</p>}
+            <button type="submit" disabled={isSubmitting || waiting} className={`mt-2 w-full bg-[#42c1fc] text-white font-semibold py-2 rounded-xl
+               hover:bg-[#1e88e5] transition-all shadow-md hover:shadow-lg  ${isSubmitting || waiting ? 'opacity-30' : ''} ${!isDark&&"text-[#4e5c77]  bg-[#b9c0ff] hover:bg-[#959fff]"} cursor-pointer`}>Enviar Email</button>
+        </form>
+    </>
   )
 }
 
